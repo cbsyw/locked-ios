fcfdb84f1de6173bfa360a847401f2ad9630d8b8
Chris Warren <101680158+cbsyw@users.noreply.github.com>
2025-12-28 22:23:08 -0500

    new

---
+++ locked/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  locked
+//
+//  Created by Christian Warren on 12/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ locked/lockedApp.swift
@@ -0,0 +1,17 @@
+//
+//  lockedApp.swift
+//  locked
+//
+//  Created by Christian Warren on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct lockedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
